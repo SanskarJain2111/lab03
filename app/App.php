e8c335052a55cf6c46669a14fcc1bd77f293c44c
SanskarJain2111 <jainsa_1@rknec.edu>
2023-11-16 22:11:47 +0530

    Changes to be committed

---
+++ app/App.php
@@ -0,0 +1,11 @@
+<?php
+namespace Company\Utility;
+
+use Company\Utility\Controller\MainController;
+
+class App {
+    public static function init() {
+        $controller = new MainController();
+        $controller->show();
+    }
+}
